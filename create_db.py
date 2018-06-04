6e440a3c57ca7c772cd0964e3e2206af57d49e0d
Guilheme Guimarães <guilherme.g.v.l.silva@gmail.com>
2018-06-04 13:15:16 -0300

    Mudança de execute para executescript no create_db

--- create_db.py
+++ create_db.py
@@ -1,24 +1,21 @@
-#Versão 1.0.0
+'''Versão 1.0.1'''
 import sqlite3 as sql
 
 con = sql.connect('py_politica.db')
-cursor = con.cursor()
 
-cursor.execute('''
-CREATE TABLE partido( 
+con.cursor().executescript('''
+	CREATE TABLE partido( 
 		id_partido varchar primary key,
 		sigla varchar not null,
 		nome varchar not null,
-		data_criacao date not null);''')
+		data_criacao date not null);
 
-cursor.execute('''
-CREATE TABLE parlamentar( 
+	CREATE TABLE parlamentar( 
 		id_parlamentar varchar primary key,
 		nome varchar not null,
-		casa char not null);''')
+		casa char not null);
 
-cursor.execute('''
-CREATE TABLE filiacao( 
+	CREATE TABLE filiacao( 
 		id_parlamentar varchar not null,
 		id_partido varchar not null,
 		data_filiacao date not null,
@@ -26,10 +23,9 @@ CREATE TABLE filiacao(
 		uf varchar not null,
 		primary key(id_parlamentar, id_partido, data_filiacao),
 		foreign key(id_parlamentar) references parlamentar(id_parlamentar),
-		foreign key(id_partido) references partido(id_partido));''')
+		foreign key(id_partido) references partido(id_partido));
 
-cursor.execute('''
-CREATE TABLE materia( 
+	CREATE TABLE materia( 
 		id_materia varchar primary key,
 		tipo varchar null,
 		autor varchar null,
@@ -37,34 +33,29 @@ CREATE TABLE materia(
 		data_apresentacao date null,
 		ementa varchar null,
 		apelido varchar null,
-		status varchar null);''')
+		status varchar null);
 	
-cursor.execute('''
 	CREATE TABLE temas_materia(
 		id_materia varchar not null,
 		tema varchar not null,
 		primary key (id_materia, tema),
-		foreign key (id_materia) references materia (id_materia);
-	''')
-
-cursor.execute('''
-CREATE TABLE votacao( 
+		foreign key (id_materia) references materia (id_materia));
+	
+	CREATE TABLE votacao( 
 		id_votacao varchar primary key,
 		id_materia varchar null,
 		dataHoraInicio datetime not null,
-		foreign key(id_materia) references materia(id_materia));''')
+		foreign key(id_materia) references materia(id_materia));
 
-cursor.execute('''
-CREATE TABLE votacao_secreta( 
+	CREATE TABLE votacao_secreta( 
 		id_votacao varchar not null,
 		placarSim integer null,
 		placarNao integer null,
 		placarAbs integer null,
 		primary key(id_votacao),
-		foreign key(id_votacao) references votacao(id_votacao));''')
-
-cursor.execute('''
-CREATE TABLE voto( 
+		foreign key(id_votacao) references votacao(id_votacao));
+		
+	CREATE TABLE voto( 
 		id_parlamentar varchar not null,
 		id_votacao varchar not null,
 		descricao integer not null,
